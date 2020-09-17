72555ab58dbc87e905b3637289ecbb4a7a2c29f9
Ferry Anggriawan <ferrytailsekai@gmail.com>
2020-09-17 16:34:12 +0700

    init

---
+++ hello-npmjs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Hello Man",
+    },
+    name: {
+      type: String,
+      default: "Ferry",
+    },
+  },
+}
+</script>
+
+<style></style>
